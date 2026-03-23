8d66305c7dbe594b4e81f052f4124186d802a18e
Shatbera <81585007+Shatbera@users.noreply.github.com>
2026-03-23 19:55:00 +0400

    #fix port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
