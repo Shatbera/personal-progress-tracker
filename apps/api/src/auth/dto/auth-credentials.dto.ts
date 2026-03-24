e67f5baa413373c653d62b1f76ecf16e514d077a
Shatbera <81585007+Shatbera@users.noreply.github.com>
2026-03-24 20:03:13 +0400

    #enable longer username length

--- apps/api/src/auth/dto/auth-credentials.dto.ts
+++ apps/api/src/auth/dto/auth-credentials.dto.ts
@@ -3,7 +3,7 @@ import { IsString, Matches, MaxLength, MinLength } from "class-validator";
 export class AuthCredentialsDto {
     @IsString()
     @MinLength(4)
-    @MaxLength(20)
+    @MaxLength(50)
     username: string;
     @IsString()
     @MinLength(8)
